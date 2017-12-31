d52af17ba72dc7fbeab75eac6994e60f8ef9c882
MRazian <razian.mr@gmail.com>
2018-01-01 02:50:06 +0330

    Add plot

--- check.sh
+++ check.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+
+[ -f ip ] || touch ip
+
 for i in $( cat iplist )
 do
 	ping -q -c2 $i > /dev/null
 	if [ $? -eq 0 ]
 	then
-		echo $i "Pingable"
-	else
-		echo $i "Not Pingable"
+		echo $i >> ip
 	fi
 done
+
+#cat ip > sort > uniq >> ip
--- run.sh
+++ run.sh
@@ -1,11 +1,39 @@
 #!/bin/sh
-for i in $( cat iplist )
+
+#backup then remove
+
+
+
+##############Ping Swip!################################
+path=results
+#rm -rf $path
+mkdir -p $path
+
+day=$(date | awk '{print 2017"-"$2"-"$3}')
+time=$(date | awk '{print $4}')
+row="$day $time "
+for i in $( cat ip )
 do
-	ping -q -c2 $i > /dev/null
+	pingOutput=$(ping -q -c2 $i)
 	if [ $? -eq 0 ]
 	then
-		echo $i "Pingable"
+		echo "$i pingable"
+	        touch $path/$i
+		date >> $path/$i
+		echo "$pingOutput" | grep avg >> $path/$i
+
+		###aggregated Data
+		pResult=$(echo "$pingOutput" | grep avg | awk -F'=' '{print $2}' | awk -F'/' '{print $2}')
+		row="$row $pResult"
 	else
-		echo $i "Not Pingable"
+		echo "$i error"
 	fi
 done
+
+ 
+#data prepartion phase
+echo $row >> $path/aggData
+
+#draw plot after each inspection
+./plot > p1.png
+eog p1.png
